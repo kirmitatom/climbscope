947eaa16270568802f4cfcf34ff06b247d9b9815
kirmitatom <125992097+kirmitatom@users.noreply.github.com>
2025-07-02 19:56:08 +0300

    started putting components in layout

---
+++ climbscope-web/componenets/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return (
+        <nav className="border-2 border-red-50 flexBetween max-container padding-container relative z-30 py-5">
+            <Link href="#"></Link>
+        </nav>
+    )
+}
+export default Navbar
